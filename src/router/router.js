4742e59a229699558625fcf50b3947ce0f00727c
fork_web <leshadgrad@mail.ru>
2020-11-25 14:25:28 +0400

    requireLogin

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 
 import LogIn from '../components/LogIn'
 import TaskBoard from '../components/TaskBoard'
+
+import firebase from 'firebase/app'
+
 Vue.use(Router);
 
 let router = new Router({
@@ -17,12 +20,24 @@ let router = new Router({
     {
       path: '/board',
       name: 'board',
-      meta: {layout: "main"},
+      meta: {layout: "main", login: true},
       component: TaskBoard,
       props: true
     }
   ]
 
 })
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requireLogin = to.matched.some(record => record.meta.login);
+
+  if(requireLogin && !currentUser) {
+    next('/');
+  } else {
+    next();
+  }
+
+});
 
 export default router;

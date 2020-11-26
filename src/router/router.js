c478559df3cc260a102792ff3551f3f2d4225dc2
fork_web <leshadgrad@mail.ru>
2020-11-26 18:11:20 +0400

    done

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 
 import LogIn from '../components/LogIn'
 import TaskBoard from '../components/TaskBoard'
+import TaskCreate from '../components/TaskCreate'
+import TaskList from '../components/TaskList'
+import Task from '../components/Task'
 
 import firebase from 'firebase/app'
 
@@ -22,6 +25,27 @@ let router = new Router({
       name: 'board',
       meta: {layout: "main", login: true},
       component: TaskBoard,
+      props: true
+    },
+    {
+      path: '/create',
+      name: 'create',
+      meta: {layout: "main", login: true},
+      component: TaskCreate,
+      props: true
+    },
+    {
+      path: '/list',
+      name: 'list',
+      meta: {layout: "main", login: true},
+      component: TaskList,
+      props: true
+    },
+    {
+      path: '/task/:id',
+      name: 'task',
+      meta: {layout: "main", login: true},
+      component: Task,
       props: true
     }
   ]
